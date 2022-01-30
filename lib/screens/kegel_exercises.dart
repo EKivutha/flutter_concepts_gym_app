98a4b85d5e5bbe7336cf007fe42f13139e1b504b
ezz <gerkibz@outlook.com>
2022-01-30 09:23:25 +0300

    todo screens

---
+++ lib/screens/kegel_exercises.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class KegelScreen extends StatelessWidget {
+  const KegelScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
